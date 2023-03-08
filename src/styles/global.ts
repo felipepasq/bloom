24dc39d7fb49cd488d553c811ea4ababb8b6245d
Felipe Pasqua <felipepasquarodrigues@gmail.com>
2023-03-07 22:49:28 -0300

    feat:installed eslint and prettier

---
+++ src/styles/global.ts
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+   * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+  
+  body  {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  a, a:hover, a:focus, a:active {
+      text-decoration: none;
+      color: inherit;
+ }
+
+  button {
+	background: none;
+	color: inherit;
+	border: none;
+	padding: 0;
+	font: inherit;
+	cursor: pointer;
+	outline: inherit;
+}
+
+`
+
+export default GlobalStyle
